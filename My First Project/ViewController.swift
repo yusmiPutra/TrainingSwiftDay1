e6d64e68a72031997259554bda400825656e790d
Udacoding Indonesia <yusmiputrawg93@gmail.com>
2023-07-24 10:42:39 +0700

    Initial Commit

---
+++ My First Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My First Project
+//
+//  Created by Yusmi Putra on 7/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
